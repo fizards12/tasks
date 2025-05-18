5b2dbc5d7d86ace94ae0f7ec21ecea19563ba752
fizards12 <mahmoudsameh734@gmail.com>
2025-05-18 15:05:08 +0300

    changes in ToDo

--- ToDo/script.js
+++ ToDo/script.js
@@ -10,10 +10,22 @@ function addTask(event) {
   row.innerHTML = `
     <td>${title}</td>
     <td title="${description}">${description}</td>
-    <td><button class="btn btn-text btn-danger" onclick="deleteRow('${row.id}')">X</button></td>
+    <td class="status"><span>To Do</span></td>
+    <td>
+      <button class="btn btn-text btn-danger" onclick="deleteRow('${row.id}')">
+        X
+      </button>
+      <button class="btn btn-text btn-success" onclick="doneTask('${row.id}')">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+        </svg>
+      </button>
+    </td>
     `;
   document.querySelector("tbody").appendChild(row);
   counter++;
+  closePopup();
+  form.reset();
 }
 function openPopup() {
   document.querySelector(".popup").setAttribute("open", "");
@@ -26,4 +38,10 @@ function closePopup() {
 function deleteRow(id) {
   const row = document.getElementById(id);
   row.remove();
+}
+
+function doneTask(id){
+  const row = document.getElementById(id);
+  const status = row.querySelector(".status span");
+  status.textContent = "Done";
 }
